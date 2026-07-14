ea74ebad255c5ce01a1bd0a47f98dd346cb2a89b
erot470-art <erot470@gmail.com>
2026-07-14 06:07:34 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-# Загружаем переменные окружения
 load_dotenv()
 
-# Токен бота
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-# ID администратора
 ADMIN_ID = int(os.getenv("ADMIN_ID", "8442222382"))
 
-# Настройки базы данных (на Render используем постоянное хранилище)
-DATABASE_NAME = os.getenv("DATABASE_NAME", "/opt/render/project/src/anonymous_bot.db")
-
-# Стоимость раскрытия личности
+# ВАЖНО: Используем /tmp на Render для записи файлов
+DATABASE_NAME = os.getenv("DATABASE_NAME", "/tmp/anonymous_bot.db")
 REVEAL_COST = float(os.getenv("REVEAL_COST", "0"))
